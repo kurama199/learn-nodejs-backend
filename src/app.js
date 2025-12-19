6abb629cfc9beb32e82bdc787a02ca0afc3f8bc5
NLP <nikhillalpawa.gmail.com>
2025-12-19 15:12:12 +0530

    19/12/25: setup middlewares, models for user and video

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(
+  express.json({
+    limit: '16kb',
+  })
+);
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+
+app.use(express.static('public'));
+app.use(cookieParser());
+
+export { app, PORT };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from 'dotenv';
 import connectToDB from './db/index.js';
+import { app, PORT } from './app.js';
 
 dotenv.config({
   path: './.env',
 });
 
-connectToDB();
+connectToDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to connect to the database:', err);
+  });
