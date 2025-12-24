e149ea6eb9274bece093ebe72833641c513854da
NLP <nikhillalpawa.gmail.com>
2025-12-24 18:20:56 +0530

    24/12: added sub aggregation and all other routes for user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { ApiResponse } from '../utils/ApiResponse.js';
 import { User } from '../models/user.model.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
 import { uploadOnCloudinary } from '../utils/cloudinary.js';
 
 const generateAccessOrRefreshToken = async (userId) => {
@@ -394,6 +395,64 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     );
 });
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate(
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: 'videos',
+        localField: 'watchHistory',
+        foreignField: '_id',
+        as: 'watchHistory',
+        pipeline: [
+          {
+            $lookup: {
+              from: 'users',
+              localField: 'owner',
+              foreignField: '_id',
+              as: 'owner',
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: '$owner',
+              },
+            },
+          },
+        ],
+      },
+    }
+  );
+
+  if (!user) {
+    throw new ApiError(500, 'error while fetching watch history');
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        'Watch History Fetched Successfully'
+      )
+    );
+});
+
 export {
   registerUser,
   loginUser,
@@ -404,4 +463,6 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,15 @@
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from '../controllers/user.controller.js';
 
 import { Router } from 'express';
@@ -23,5 +30,16 @@ router.route('/login').post(loginUser);
 //secured routes
 router.route('/logout').post(verifyJWT, logoutUser);
 router.route('/refresh-access-token').post(refreshAccessToken);
+router.route('/change-password').post(verifyJWT, changeCurrentPassword);
+router.route('/user-details').get(verifyJWT, getCurrentUser);
+router.route('/update-account').patch(verifyJWT, updateAccountDetails);
+router
+  .route('/update-avatar')
+  .patch(verifyJWT, upload.single('avatar'), updateUserAvatar);
+router
+  .route('/update-cover-image')
+  .patch(verifyJWT, upload.single('coverImage'), updateUserCoverImage);
+router.route('/c/:username').get(verifyJWT, getUserChannelProfile);
+router.route('watch-history').get(verifyJWT, getWatchHistory);
 
 export default router;
